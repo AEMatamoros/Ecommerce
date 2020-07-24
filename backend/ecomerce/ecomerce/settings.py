d133a29b8ad50aebf3229b8611d90d59c4429715
Alexis <Alexismatamoros1996888@gmail.com>
2020-07-24 16:22:40 -0600

    Backend COnfigurado para Heroku
    NO;NO,NO hacer freze de los requerimientos si no es necesario, puede provocar muchos problemas, por los requerimientos del entorno virtual

--- backend/ecomerce/ecomerce/settings.py
+++ backend/ecomerce/ecomerce/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+#Heroku Settings
+import django_heroku
 
 import os
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'wamc8dpv4tb&gfz!565%xn9(##pxo7mpsxw9g-88sccm4y*35o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["api-commerce-store.herokuapp.com"]
 
 AUTH_USER_MODEL = 'api.Account'
 
@@ -61,6 +63,7 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -95,35 +98,11 @@ WSGI_APPLICATION = 'ecomerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}"""
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'industria',
-        'USER': 'postgres',
-        'PASSWORD': 'Control',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'industria',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-"""
+#HerokuDB
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES = {'default': dj_database_url.config()}
+DATABASES['default'].update(prod_db)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -161,7 +140,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #Media
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+
+
+
