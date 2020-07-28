15c59f4b7af4102c6d1af41bf39cd79aa8d02ba3
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-07-28 11:24:40 -0600

    rutas hijas

--- frontend/ecommerce/src/app/pages/home/home.routes.ts
+++ frontend/ecommerce/src/app/pages/home/home.routes.ts
@@ -7,11 +7,22 @@ import { AuthGuard } from '../../guards/auth.guard';
 import { PerfilComponent } from './perfil/perfil.component';
 import { MyproductsComponent } from './products/myproducts/myproducts.component';
 import { ComprasComponent } from './compras/compras.component';
+import { DetalleProductoComponent } from './detalle-producto/detalle-producto.component';
 
 const pagesHomeRoutes: Routes = [
     {path: 'perfil', component: PerfilComponent, canActivate: [ AuthGuard ]},
-    {path: 'compras', component: ComprasComponent},
-    {path: 'misProductos', component: MyproductsComponent, canActivate: [AuthGuard]}
+    {
+        path: 'compras',
+        component: ComprasComponent,
+        children: [
+            {path: 'detalle', component: DetalleProductoComponent}
+        ]
+    },
+    {
+        path: 'misProductos',
+        component: MyproductsComponent,
+        canActivate: [AuthGuard]
+    }
 ];
 
 export const PAGES_HOME_ROUTES = RouterModule.forChild( pagesHomeRoutes );
