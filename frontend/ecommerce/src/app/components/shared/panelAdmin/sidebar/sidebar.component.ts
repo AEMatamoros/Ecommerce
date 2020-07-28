8efa07465c3945309592dafbc9d86b5ec2eb4d31
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-07-28 15:26:40 -0600

    actualizacion de scripts

--- frontend/ecommerce/src/app/components/shared/panelAdmin/sidebar/sidebar.component.ts
+++ frontend/ecommerce/src/app/components/shared/panelAdmin/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SidebarService } from '../../../../services/shared/sidebar.service';
+import { UserService } from 'src/app/services/auth/user.service';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -8,7 +11,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public _sidebar: SidebarService,
+    public auth: UserService) { }
 
   ngOnInit(): void {
   }
--- frontend/ecommerce/src/app/pages/home/home.component.ts
+++ frontend/ecommerce/src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare function initPlugins();
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './home.component.html',
@@ -10,6 +12,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    initPlugins();
   }
 
 }
