61c838b413909617c172e3533ffdc749be6e6626
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-07-30 16:02:59 -0600

    arreglando cagada

--- frontend/ecommerce/src/app/app-routing.module.ts
+++ frontend/ecommerce/src/app/app-routing.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeRoutingModule } from './pages/home/home.routing'
+import { AdminRoutingModule } from './pages/panel-admin/panel-admin.routing';
 //Global Pages
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
-import { PerfilComponent } from './pages/perfil/perfil.component';
-import { UsuarioComponent } from './pages/usuario/usuario.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { ComprasComponent } from './pages/compras/compras.component';
 
-//Componentes
-import{ InformacionComponent } from './pages/home/informacion/informacion.component';
 
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent },
-  {path: 'usuario', component: UsuarioComponent },
   {path: 'registro', component: RegisterComponent},
   {path: 'login', component: LoginComponent }
 ];
--- frontend/ecommerce/src/app/app.module.ts
+++ frontend/ecommerce/src/app/app.module.ts
@@ -7,18 +7,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { LoginComponent } from './components/auth/login/login.component';
-import { PerfilComponent } from './pages/perfil/perfil.component';
-import { ComprasComponent } from './pages/compras/compras.component';
+
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { RegisterComponent } from './components/auth/register/register.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { UpdateComponent } from './pages/products/update/update.component';
-import { MyproductsComponent } from './pages/products/myproducts/myproducts.component';
-import { DeleteComponent } from './pages/products/delete/delete.component';
-import { CreateComponent } from './pages/products/create/create.component';
-import { UsuarioComponent } from './pages/usuario/usuario.component';
 
 //Componentes
 import { LoginComponent } from './components/auth/login/login.component';
@@ -33,13 +24,7 @@ import { SharedHomeModule } from './components/shared/shared.module';
   declarations: [
     AppComponent,
     LoginComponent,
-    PerfilComponent,
-    RegisterComponent,
-    UpdateComponent,
-    MyproductsComponent,
-    DeleteComponent,
-    CreateComponent,
-    UsuarioComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
