c32e8eef65c2288aae301b8d93ddc029c4cfc37c
adonay <adonay16.vz@gmail.com>
2020-08-18 02:52:34 -0600

    Carrusel en home sin terminar

--- frontend/ecommerce/src/app/components/shared/cards/cards.component.ts
+++ frontend/ecommerce/src/app/components/shared/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShopService } from '../../../services/shop/shop.service';
 
 @Component({
   selector: 'app-cards',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
-  constructor() { }
+  public productos;
+  public productosImagenes;
+  public productosCarrusel = [];
+  public c = 1;
+
+
+  constructor( private shopService:ShopService ) { }
 
   ngOnInit(): void {
+    this.getDatos();
   }
+
+  getDatos() {
+    this.shopService.getProducts()
+      .subscribe(data => {
+        this.productos = data;
+        // console.log(this.productos);
+
+        this.shopService.getProductsImage()
+          .subscribe(data => {
+          this.productosImagenes = data;
+          // console.log(this.productosImagenes);
+          while (this.c <= 4 ) {
+            // console.log('hola beibe', this.c);
+            // console.log(this.productos[this.c].id);
+            for (let prodImg of this.productosImagenes) {
+              if (prodImg.product_id.id === this.productos[this.c].id) {
+                this.productosCarrusel.push(prodImg);
+                break;
+              }
+            }
+            this.c += 1;
+          }
+          console.log(this.productosCarrusel);
+
+          });
+      });
+
+  }
+
 
 }
