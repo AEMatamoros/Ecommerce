ac55d64a6670656aaec4468f92d680e29909c792
Alexis <Alexismatamoros1996888@gmail.com>
2020-08-24 17:26:59 -0600

    Try Push this shit

--- servidor-notificaciones/server.js
+++ servidor-notificaciones/server.js
@@ -10,7 +10,32 @@ const fakeDatabase = []
 
 const app = express()
 
+//Coors
 app.use(cors())
+app.use(cors({origin: 'https://e-phoenixstoreunah.web.app/landing'}));
+
+
+/*
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://e-phoenixstoreunah.web.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+*/
+
 //Parse Json
 app.use(bodyParser.urlencoded({
   extended: true
@@ -18,6 +43,8 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 webpush.setVapidDetails('mailto:you@domain.com', PUBLIC_VAPID, PRIVATE_VAPID)
+
+
 
 //Test
 app.get('', (req, res) => {
@@ -30,6 +57,7 @@ app.post('/subscription', (req, res) => {
     fakeDatabase.push(subscription)
     console.log(fakeDatabase)
     console.log(req.body)
+    res.status(201).json({})
 })
 
 app.post('/sendNotification', (req, res) => {
