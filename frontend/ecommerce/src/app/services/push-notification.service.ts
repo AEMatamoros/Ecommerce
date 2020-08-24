22eb76cf345d502b1bb69d87263eab2430748575
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-08-24 08:24:25 -0600

    cambio url de push notificacions

--- frontend/ecommerce/src/app/services/push-notification.service.ts
+++ frontend/ecommerce/src/app/services/push-notification.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Subscription } from 'rxjs';
-const SERVER_URL = 'http://localhost:3000/subscription';
+//const SERVER_URL = 'http://localhost:3000/subscription';
+const SERVEL_URL = 'https://unah-push-notifications.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
